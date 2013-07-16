d9c0b50b3d9d3f342c614cadff8ed6a8b44e5b78
Debora Fernandes <dfernandes@corp.hotwords.com>
2013-07-16 09:49:19 -0300

    Iniciando...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Agenda::Application.config.secret_token = '41e55a04ca46b5c42f33999af268e55c91ac832cb7df343dca69b0d0649080cac509c3cc77d8c01a8bbb3477ac601294a5f4dcfb3cb9cc5d0d7c884d971089b8'
